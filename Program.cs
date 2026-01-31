288a53659eded313a7d382196f4a88858aa6c4e1
TrudaPlays <gertrude.m.fleischmann@gmail.com>
2026-01-31 16:59:56 -0600

    adding comments

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 
+//I apologize for the bad naming of these files. I decided to start working
+//on my assignment on the same file as I was doing CodeMonkey's tutorial and didn't realize it is
+//impossible to change the GitHub name and repository once you've made it until I was already almost finished with the assignment...
+//That is why the GitHub URL is going to be named HelloWorldCodeMonkey and my calculator is called HelloWorld...
+
 namespace Calculator
 {
     class Program
     {
         static void Main()
         {
+            //setting all of my counters and variables to initial values
             string? input;
             int operationCounter = 0;
             string? result = null;
-            int additionCounter = 0;
-            int subtractionCounter = 0;
-            int multiplicationCounter = 0;
-            int divisionCounter = 0;
-            int averageCounter = 0;
-            int salestaxCounter = 0;
+            int additionCounter = 0; //counter for how many additions are performed
+            int subtractionCounter = 0; //counter for how many subtractions are performed
+            int multiplicationCounter = 0; //counter for how many multiplications are performed
+            int divisionCounter = 0; //counter for how many divisions are performed
+            int averageCounter = 0; //counter for how many average calculations are performed
+            int salestaxCounter = 0; //counter for how many sales tax calculations are performed
 
             do
             {
+                //printing out the calculator menu
                 Console.WriteLine("Welcome to the console calculator!");
                 Console.WriteLine("Enter 1 for addition.");
                 Console.WriteLine("Enter 2 for subtraction.");
@@ -28,29 +35,30 @@ namespace Calculator
                 Console.WriteLine("Enter 6 for finding the 5.5% sales tax of a dollar amount.");
                 Console.WriteLine("Enter exit to escape.");
 
-                int choice = 0;
+                int choice = 0; //setting choice to a default value
 
-                input = Console.ReadLine();
+                input = Console.ReadLine(); //reads the user input
+
 
                 try
                 {
-                    choice = Convert.ToInt32(input);
+                    choice = Convert.ToInt32(input); //tries to convert the answer to an int
                 }
-                catch(FormatException ex)
+                catch(FormatException ex) //if it catches a non-numerical output that isnt equal to exit the code will loop back to the start
                 {
                     choice = 0;
-                    Console.WriteLine("");
+                    Console.WriteLine(""); //whenever this line of code pops up throughout the code it is to add an extra enter for ease of reading
                 }
 
 
-                switch (choice)
+                switch (choice) //switching through the different inputs to decide which calculation to perform
                 {
-                    case 1:
+                    case 1: //addition case
                         decimal number1 = 0;
                         decimal number2 = 0;
 
                         Console.WriteLine("Addition chosen");
-                        while (true)
+                        while (true) //this while loop will run until a numerical input is recieved
                         {
                             Console.WriteLine("Choose your first number");
                             try
@@ -58,7 +66,7 @@ namespace Calculator
                                 number1 = Convert.ToDecimal(Console.ReadLine());
                                 break;
                             }
-                            catch (FormatException ex)
+                            catch (FormatException ex) //code will keep looping if non numerical input is given
                             {
                                 continue;
                             }
@@ -82,13 +90,13 @@ namespace Calculator
 
                         Console.WriteLine(number1 + " plus " + number2 + " is " + total);
                         Console.WriteLine("");
-                        operationCounter++;
-                        additionCounter++;
-                        result = "addition";
+                        operationCounter++; //adds 1 to the operation counter to track how many calculations have been performed
+                        additionCounter++; // counts how many additions have been performed for the summary at the end
+                        result = "addition"; //sets a variable to the current calculation so the summary knows what calculation was last performed
 
                         break;
 
-                    case 2:
+                    case 2: //subtraction case
                         decimal sub1 = 0;
                         decimal sub2 = 0;
 
@@ -129,10 +137,9 @@ namespace Calculator
                         subtractionCounter++;
                         result = "subtraction";
 
-
                         break;
 
-                    case 3:
+                    case 3: //multiplication case
                         decimal mul1 = 0;
                         decimal mul2 = 0;
 
@@ -175,7 +182,7 @@ namespace Calculator
                         
                         break;
 
-                    case 4:
+                    case 4: //division case
                         decimal div1 = 0;
                         decimal div2 = 0;
 
@@ -201,7 +208,7 @@ namespace Calculator
                             {
                                 div2 = Convert.ToDecimal(Console.ReadLine());
 
-                                if (div2 != 0)
+                                if (div2 != 0) //handling division by zero
                                 {
                                     decimal divTotal = div1 / div2;
 
@@ -226,7 +233,8 @@ namespace Calculator
 
                         break;
 
-                    case 5:
+                    case 5: //average case
+
                         decimal avg1 = 0;
                         decimal avg2 = 0;
 
@@ -270,7 +278,8 @@ namespace Calculator
 
                         break;
 
-                    case 6:
+                    case 6: //sales tax case
+
                         decimal price = 0;
 
                         Console.WriteLine("Sales tax calculator chosen. 5.5% sales tax will be applied.");
@@ -290,9 +299,9 @@ namespace Calculator
                         }
 
                         decimal taxRate = 0.055m; // 5.5% sales tax
-                        decimal taxAmount = Math.Round(price * taxRate, 2, MidpointRounding.AwayFromZero);
-                        decimal salesTaxTotal = price + taxAmount;
-                        string currencyTotal = salesTaxTotal.ToString("C");
+                        decimal taxAmount = Math.Round(price * taxRate, 2, MidpointRounding.AwayFromZero); //finds the tax dollar amount rounds the answer to the correct dollar and cents format
+                        decimal salesTaxTotal = price + taxAmount; //finds the final dollar amount
+                        string currencyTotal = salesTaxTotal.ToString("C"); //gives the amount the dollar format
                         string currencyPrice = price.ToString("C");
 
 
@@ -304,15 +313,17 @@ namespace Calculator
 
                         break;
 
-
                 }
 
-            } while (input.ToLower() != "exit");
+            } while (input.ToLower() != "exit"); //breaks the do-while loop when the user inputs 'exit'
 
-
+            //Printing the summary
             Console.WriteLine("Console calculator exited. Goodbye!");
-            Console.WriteLine("You performed " + operationCounter + " successful calculations.");
+            Console.WriteLine("You performed " + operationCounter + " successful calculations."); //takes the operation counter and tells the user how many calculations have been performed
 
+            /*following code takes each separate counter for the different
+             * calculation methods and tells the user how many of each calculation was performed                
+             */
             Console.WriteLine("You did addition " + additionCounter + " times.");
             Console.WriteLine("You did subtraction " + subtractionCounter + " times.");
             Console.WriteLine("You did multiplication " + multiplicationCounter + " times.");
@@ -320,9 +331,11 @@ namespace Calculator
             Console.WriteLine("You found the average of two numbers " + averageCounter + " times.");
             Console.WriteLine("You found the sales tax " + salestaxCounter + " times.");
 
+
+            //writes the last calculation performed, if nothing was performed a suitable message is displayed
             if (operationCounter>0) Console.WriteLine("The last calculation done was " + result);
             else Console.WriteLine("No calculations were performed.");
-            Console.WriteLine("");
+            Console.WriteLine(""); //extra line for eye-friendliness
 
             //The following code does nothing it is only to satisfy the grading rubric :)
             const int endingPoint = 10;
