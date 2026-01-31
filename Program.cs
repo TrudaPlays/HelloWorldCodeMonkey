87027e3058fb5b0c16afebd1cb42a997d8fb7376
TrudaPlays <gertrude.m.fleischmann@gmail.com>
2026-01-31 14:57:56 -0600

    adding summary

--- Program.cs
+++ Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace Calculator
 {
     class Program
     {
         static void Main()
         {
+            string? input;
+            int operationCounter = 0;
+            string result = null;
 
-            while (true) 
+            do
             {
                 Console.WriteLine("Welcome to the console calculator!");
                 Console.WriteLine("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
-                Console.WriteLine("Enter avg for average of two numbers, and tax for finding the 5.5% sales tax of a dollar amount");
+                Console.WriteLine("Enter 5 for average of two numbers, and 6 for finding the 5.5% sales tax of a dollar amount");
+                Console.WriteLine("Enter exit to escape.");
 
-                int choice = 1;
-                string input;
+                int choice = 0;
 
                 input = Console.ReadLine();
-                choice = Convert.ToInt32(input);
+
+                try
+                {
+                    choice = Convert.ToInt32(input);
+                }
+                catch(FormatException ex)
+                {
+                    choice = 0;
+                    Console.WriteLine("");
+                }
 
 
                 switch (choice)
@@ -36,6 +48,8 @@ namespace HelloWorld
 
                         Console.WriteLine(number1 + " plus " + number2 + " = " + total);
                         Console.WriteLine("");
+                        operationCounter++;
+                        result = "addition";
 
 
                         break;
@@ -54,6 +68,8 @@ namespace HelloWorld
 
                         Console.WriteLine(sub1 + " minus " + sub2 + " = " + subTotal);
                         Console.WriteLine("");
+                        operationCounter++;
+                        result = "subtraction";
 
 
                         break;
@@ -72,6 +88,8 @@ namespace HelloWorld
 
                         Console.WriteLine(mul1 + " times " + mul2 + " = " + mulTotal);
                         Console.WriteLine("");
+                        operationCounter++;
+                        result = "multiplication";
 
 
                         break;
@@ -92,19 +110,70 @@ namespace HelloWorld
 
                             Console.WriteLine(div1 + " divided by " + div2 + " = " + divTotal);
                             Console.WriteLine("");
+                            operationCounter++;
+                            result = "division";
                         }
                         else
                         {
                             Console.WriteLine("Sorry divison by zero is not allowed.");
+                            Console.WriteLine(" ");
                         }
+                        
 
 
                         break;
 
+                    case 5:
+                        decimal avg1 = 0;
+                        decimal avg2 = 0;
+
+                        Console.WriteLine("Average of two numbers chosen");
+                        Console.WriteLine("Choose your first number");
+                        avg1 = Convert.ToDecimal(Console.ReadLine());
+                        Console.WriteLine("Choose your second number");
+                        avg2 = Convert.ToDecimal(Console.ReadLine());
+
+                        decimal avgTotal = (avg1 + avg2) / 2;
+
+                        Console.WriteLine("The average of " + avg1 + " and " + avg2 + " is " + avgTotal);
+                        Console.WriteLine(" ");
+                        operationCounter++;
+                        result = "average of two numbers";
+
+
+                        break;
+
+                    case 6:
+                        decimal price = 0;
+
+                        Console.WriteLine("Sales tax calculator chosen. 5.5% sales tax will be applied.");
+                        Console.WriteLine("Enter your dollar amount");
+                        price = Convert.ToDecimal(Console.ReadLine());
+
+                        decimal taxRate = 0.055m; // 5.5% sales tax
+                        decimal taxAmount = Math.Round(price * taxRate, 2, MidpointRounding.AwayFromZero);
+                        decimal salesTaxTotal = price + taxAmount;
+                        string currencyTotal = salesTaxTotal.ToString("C");
+                        string currencyPrice = price.ToString("C");
+
+
+                        Console.WriteLine(currencyPrice + " with 5.5% sales tax applied is equal to " + currencyTotal);
+                        Console.WriteLine(" ");
+                        operationCounter++;
+                        result = "sales tax calculation for " + currencyPrice;
+
+                        break;
 
 
                 }
-            }
+
+            } while (input.ToLower() != "exit");
+
+
+            Console.WriteLine("Console calculator exited. Goodbye!");
+            Console.WriteLine(operationCounter);
+            
+
             
 
         }
